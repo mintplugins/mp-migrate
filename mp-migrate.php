df65835577a8546e3a6a010da95a467e72ab2d0e
Phil Johnston <johnstonphilip@gmail.com>
2013-06-30 02:41:15 -0400

    1.0.0.0
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- mp-migrate.php
+++ mp-migrate.php
@@ -3,7 +3,7 @@
 Plugin Name: MP Migrate
 Plugin URI: http://moveplugins.com
 Description: Migration  of settings from Mint Themes to Move Plugins
-Version: 1.0
+Version: 1.0.0.0
 Author: Move Plugins
 Author URI: http://moveplugins.com
 Text Domain: mp_migrate
